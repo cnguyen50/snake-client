f50a6fede997dffa3a3f01f25b8379a31cc32e4f
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-17 01:32:04 +0000

    imported

--- play.js
+++ play.js
@@ -1,29 +1,8 @@
-const net = require('net');
+const { connect } = require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('error', (err) => {
-    console.log(err);
-  });
-
-  
-
-
-  return conn;
-
-
-  
-}
-
+const { setUpInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
+
+setUpInput();
