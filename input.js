033298508492a5829c2d9e655c5e9aa55040764f
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-17 02:42:41 +0000

    say:YUR

---
+++ input.js
@@ -0,0 +1,34 @@
+let connection;
+
+const handleUserInput = function(data) {
+
+    if (data === '\u0003') {
+        process.exit();
+    } else if (data === 'w') {
+        connection.write('Move: up');
+    } else if (data === 'a') {
+        connection.write('Move: left');
+    } else if (data === 's') {
+        connection.write('Move: down');
+    } else if (data === 'd') {
+        connection.write('Move: right');
+    } else if (data === '1') {
+        connection.write('Say: YUR');
+    }
+}
+
+
+const setUpInput = function(conn) {
+    connection = conn;
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', handleUserInput);
+    return stdin;
+}
+
+
+
+
+module.exports = { setUpInput };
