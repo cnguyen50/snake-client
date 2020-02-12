61c19c22f3dd44eb470263ac3d43f89d4ed6b9a6
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-12 20:54:58 +0000

    exported connect into play.js

--- client.js
+++ client.js
@@ -1,31 +1,69 @@
 const net = require('net');
-const { IP, PORT } = require('./constants');
 
-/**
- * Establishes connection with the game server
- */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: IP,
-    port: PORT
+    host: '192.168.88.177',
+    port: 50541
   });
-  conn.setEncoding('utf8');
 
-    conn.on('connect', () => {
-        console.log("Successfully connected to game server");
-    conn.write("Name:CNN");
-});
-
+  conn.setEncoding('utf8'); 
+  
   conn.on('data', (data) => {
-    console.log('Server: ', data);
-  });
-
-
-
+      console.log("Server says:", data)
+  })
 
   return conn;
-
-
 }
 
-module.exports = { connect };
+// console.log('Connecting ...');
+// connect();
+
+
+module.exports = connect;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const net = require('net');
+// const { IP, PORT } = require('./constants');
+
+// /**
+//  * Establishes connection with the game server
+//  */
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: IP,
+//     port: PORT
+//   });
+//   conn.setEncoding('utf8');
+
+//     conn.on('connect', () => {
+//         console.log("Successfully connected to game server");
+//     conn.write("Name:CNN");
+// });
+
+//   conn.on('data', (data) => {
+//     console.log('Server: ', data);
+//   });
+//   return conn;
+// }
+
+// module.exports = { connect };
