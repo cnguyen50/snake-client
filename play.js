1bf9711de6c8138677f12c1a427af035ca972ca0
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-12 20:50:19 +0000

    add event handler when idled

--- play.js
+++ play.js
@@ -1,8 +1,61 @@
-const { connect } = require('./client');
+const net = require('net');
 
-const { setUpInput } = require('./input');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.177',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8'); 
+  
+  conn.on('data', (data) => {
+      console.log("Server says:", data)
+  })
+
+  return conn;
+}
 
 console.log('Connecting ...');
-const conn = connect();
+connect();
 
-setUpInput(conn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { connect } = require('./client');
+
+// const { setUpInput } = require('./input');
+
+// console.log('Connecting ...');
+// const conn = connect();
+
+// setUpInput(conn);
